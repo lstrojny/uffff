9675fc180b19202309a80805ba3adc52e11542d2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-03-20 08:34:56 +0100

    refactor: update `flake.nix` file and use `flake.parts`
    `flake.parts` replaces `flake-utils`. This is a better practice to use it.
    I also removed inputs that were not really used.

--- flake.nix
+++ flake.nix
@@ -4,18 +4,29 @@
   inputs = {
     # Use fork until https://github.com/NixOS/nixpkgs/pull/221845 is merged
     nixpkgs.url = "github:lstrojny/nixpkgs/fix-php-wrapper";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     nix-shell.url = "github:loophp/nix-shell";
   };
 
-  outputs = { self, nixpkgs, darwin, flake-utils, nix-shell }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # This flake is for Linux (x86) and Apple (darwin) systems
+      # If you need more systems, inspect `nixpkgs.lib.systems.flakeExposed` and
+      # add them to this list.
+      #
+      # $ nix repl "<nixpkgs>"
+      # nix-repl> lib.systems.flakeExposed
+      systems = ["x86_64-linux" "aarch64-linux"];
 
-        php = (nix-shell.api.makePhp system {
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        php = inputs.nix-shell.api.makePhp system {
           php = "php82";
           withExtensions = [
             "xdebug"
@@ -36,20 +47,22 @@
           extraConfig = ''
             xdebug.mode = coverage
           '';
-        });
-      in {
-        devShells = {
-          default = pkgs.mkShellNoCC {
-            name = "ufff";
-
-            buildInputs = [
-              php
-              php.packages.composer
-              pkgs.mailhog
-              pkgs.sphinx
-              (pkgs.python3.withPackages (p: with p; [ sphinx-rtd-theme ]))
-            ];
-          };
         };
-      });
+      in {
+        # Run `nix fmt` to reformat the nix files
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShellNoCC {
+          name = "ufff";
+
+          buildInputs = [
+            php
+            php.packages.composer
+            pkgs.mailhog
+            pkgs.sphinx
+            (pkgs.python3.withPackages (p: with p; [ sphinx-rtd-theme ]))
+          ];
+        };
+      };
+    };
 }
