68fb1b27191c451d7572d24b5104be55dbdcfff7
Lars Strojny <lars@strojny.net>
2023-03-21 10:32:28 +0100

    refactor: update `flake.nix` file and use `flake.parts`
    `flake.parts` replaces `flake-utils`. This is a better practice to use it.
    I also removed inputs that were not really used.
    
    Revised contribution from https://github.com/drupol

--- flake.nix
+++ flake.nix
@@ -4,52 +4,59 @@
   inputs = {
     # Use fork until https://github.com/NixOS/nixpkgs/pull/221845 is merged
     nixpkgs.url = "github:lstrojny/nixpkgs/fix-php-wrapper";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     nix-shell.url = "github:loophp/nix-shell";
   };
 
-  outputs = { self, nixpkgs, darwin, flake-utils, nix-shell }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # This flake is for Linux (x86) and Apple (darwin) systems
+      # If you need more systems, inspect `nixpkgs.lib.systems.flakeExposed` and
+      # add them to this list.
+      #
+      # $ nix repl "<nixpkgs>"
+      # nix-repl> lib.systems.flakeExposed
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        php = (nix-shell.api.makePhp system {
-          php = "php82";
-          withExtensions = [
-            "xdebug"
-            "mbstring"
-            "filter"
-            "intl"
-            "openssl"
-            "dom"
-            "tokenizer"
-            "xmlwriter"
-            "simplexml"
-            "opcache"
-            "curl"
-            "zlib"
-            "ctype"
-          ];
+      perSystem = { pkgs, system, ... }:
+        let
+          php = inputs.nix-shell.api.makePhp system {
+            php = "php82";
+            withExtensions = [
+              "ctype"
+              "curl"
+              "dom"
+              "filter"
+              "intl"
+              "mbstring"
+              "opcache"
+              "openssl"
+              "simplexml"
+              "tokenizer"
+              "xmlwriter"
+              "zlib"
 
-          extraConfig = ''
-            xdebug.mode = coverage
-          '';
-        });
-      in {
-        devShells = {
-          default = pkgs.mkShellNoCC {
+              # Development only
+              "xdebug"
+            ];
+
+            extraConfig = ''
+              xdebug.mode = coverage
+            '';
+          };
+        in {
+          # Run `nix fmt` to reformat the nix files
+          formatter = pkgs.nixfmt;
+
+          devShells.default = pkgs.mkShellNoCC {
             name = "ufff";
 
             buildInputs = [
               php
               php.packages.composer
-              pkgs.mailhog
               pkgs.sphinx
               (pkgs.python3.withPackages (p: with p; [ sphinx-rtd-theme ]))
             ];
           };
         };
-      });
+    };
 }
