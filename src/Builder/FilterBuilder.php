59146b7188e6554a06990d56e537a839d35d62a8
Lars Strojny <lars@strojny.net>
2023-06-27 18:11:42 +0200

    Minimal repro case

--- src/Builder/FilterBuilder.php
+++ src/Builder/FilterBuilder.php
@@ -1,118 +1,36 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Uffff\Builder;
 
-use Uffff\Contract\Filter;
-use Uffff\Filter\AssertWellFormedUnicode;
-use Uffff\Filter\BalanceBidirectionalMarker;
-use Uffff\Filter\HarmonizeNewlines;
-use Uffff\Filter\NormalizeForm;
-use Uffff\Filter\StripNullByte;
-use Uffff\Filter\TrimWhitespace;
-use Uffff\Value\Newline;
-use Uffff\Value\NormalizationForm;
-use Uffff\Value\ValueContext;
-
-/**
- * @phpstan-import-type FilterFn from Filter
- */
-final class FilterBuilder
+class FilterBuilder
 {
-    private NormalizationForm $normalizationForm = NormalizationForm::NFC;
-
-    private Newline $newline = Newline::UNIX;
-
-    private bool $trimWhitespace = true;
-
     /**
-     * @var list<FilterFn>
-     */
-    private array $filters = [];
-
-    public function normalizeForm(NormalizationForm $normalizationForm): self
-    {
-        $this->normalizationForm = $normalizationForm;
-
-        return $this;
-    }
-
-    public function harmonizeNewlines(Newline $newline): self
-    {
-        $this->newline = $newline;
-
-        return $this;
-    }
-
-    public function trimWhitespace(bool $trim): self
-    {
-        $this->trimWhitespace = $trim;
-
-        return $this;
-    }
-
-    /**
-     * @param FilterFn $filter
-     */
-    public function add(callable $filter): self
-    {
-        $this->filters[] = $filter;
-
-        return $this;
-    }
-
-    /**
-     * @psalm-mutation-free
-     * @return FilterFn
+     * @return callable(string):string
      */
     public function build(): callable
     {
-        $filters = array_map(
-            [self::class, 'shortCircuitIfEmpty'],
-            [
-                FilterFactory::create(StripNullByte::class),
-                FilterFactory::createWith(
-                    AssertWellFormedUnicode::class,
-                    [ValueContext::INPUT],
-                    ValueContext::INPUT->name
-                ),
-                FilterFactory::createWith(
-                    NormalizeForm::class,
-                    [$this->normalizationForm],
-                    $this->normalizationForm->name
-                ),
-                FilterFactory::createWith(HarmonizeNewlines::class, [$this->newline], $this->newline->name),
-                ...$this->trimWhitespace ? [FilterFactory::create(TrimWhitespace::class)] : [],
-                FilterFactory::create(BalanceBidirectionalMarker::class),
-                ...$this->filters,
-                FilterFactory::createWith(
-                    AssertWellFormedUnicode::class,
-                    [ValueContext::OUTPUT],
-                    ValueContext::OUTPUT->name
-                ),
-            ]
-        );
-
-        return self::shortCircuitIfEmpty(
-            static fn (string $text): string => array_reduce(
-                $filters,
-                /**
-                 * @param FilterFn $filter
-                 */
-                static fn (string $text, callable $filter): string => $filter($text),
-                $text
-            )
-        );
+        return static fn(string $v) => $v;
     }
+}
 
-    /**
-     * @psalm-pure
-     * @param FilterFn $filter
-     * @return FilterFn
-     */
-    private static function shortCircuitIfEmpty(callable $filter): callable
+enum Newline: string
+{
+    case UNIX = "\n";
+    case WINDOWS = "\r\n";
+    case MAC = "\r";
+}
+
+class HarmonizeNewlines
+{
+    // Order matters, Windows must come first
+    private const ANY_NEWLINE = '/(?:' . Newline::WINDOWS->value . '|' . Newline::MAC->value . '|' . Newline::UNIX->value . ')/';
+
+    public function __invoke(string $text): string
     {
-        return static fn (string $text): string => $text === '' ? $text : $filter($text);
+        $harmonized = preg_replace(self::ANY_NEWLINE, Newline::UNIX->value, $text);
+
+        assert(is_string($harmonized));
+
+        return $harmonized;
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -2,72 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Uffff;
+namespace Foo;
 
 use Uffff\Builder\FilterBuilder;
 
-/**
- * @psalm-pure
- * @psalm-immutable
- */
 function unicode(string $text): string
 {
-    /**
-     * @psalm-suppress ImpureStaticVariable
-     * @var (pure-callable(string): string)|null $filter
-     */
-    static $filter = null;
-
-    $filter ??= (new FilterBuilder())
-        ->build();
+    $filter = (new FilterBuilder())->build();
 
     return $filter($text);
-}
-
-/**
- * @psalm-pure
- * @psalm-immutable
- * @return ($text is null ? null : string)
- */
-function unicode_or_null(?string $text): ?string
-{
-    if ($text === null) {
-        return null;
-    }
-
-    return unicode($text);
-}
-
-/**
- * @psalm-pure
- * @psalm-immutable
- */
-function unicode_untrimmed(string $text): string
-{
-    /**
-     * @psalm-suppress ImpureStaticVariable
-     * @var (pure-callable(string): string)|null $filter
-     */
-    static $filter = null;
-
-    /** @psalm-suppress ImpureMethodCall */
-    $filter ??= (new FilterBuilder())
-        ->trimWhitespace(false)
-        ->build();
-
-    return $filter($text);
-}
-
-/**
- * @psalm-pure
- * @psalm-immutable
- * @return ($text is null ? null : string)
- */
-function unicode_untrimmed_or_null(?string $text): ?string
-{
-    if ($text === null) {
-        return null;
-    }
-
-    return unicode_untrimmed($text);
 }
