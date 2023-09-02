0736f63b7ae96b367e14610de188d061c464c83a
Lars Strojny <lars@strojny.net>
2023-09-02 10:48:35 +0200

    Update dev env

--- flake.nix
+++ flake.nix
@@ -1,67 +1,42 @@
 {
-  description = "ufff";
+  description = "Example flake for PHP development";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nix-shell.url = "github:loophp/nix-shell";
-    nix-shell.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-php-composer-builder.url = "github:loophp/nix-php-composer-builder";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs = inputs@{ self, flake-parts, systems, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      # This flake is for Linux (x86) and Apple (darwin) systems
-      # If you need more systems, inspect `nixpkgs.lib.systems.flakeExposed` and
-      # add them to this list.
-      #
-      # $ nix repl "<nixpkgs>"
-      # nix-repl> lib.systems.flakeExposed
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = import systems;
 
-      perSystem = { pkgs, system, ... }:
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }:
         let
-          php = inputs.nix-shell.api.makePhp system {
-            php = "php82";
-            withExtensions = [
-              "ctype"
-              "curl"
-              "dom"
-              "filter"
-              "intl"
-              "mbstring"
-              "opcache"
-              "openssl"
-              "simplexml"
-              "tokenizer"
-              "xmlwriter"
-              "zlib"
-
-              # Development only
-              "xdebug"
-            ];
-
-            extraConfig = ''
-              xdebug.mode = coverage
-            '';
-          };
+          php = version:
+            pkgs.api.buildPhpFromComposer {
+              src = inputs.self;
+              php = pkgs."php${version}";
+              withExtensions = [
+                (pkgs.php83.extensions.xdebug.overrideAttrs (oldAttrs: {
+                  src = pkgs.fetchFromGitHub {
+                    owner = "xdebug";
+                    repo = "xdebug";
+                    rev = "7d10e631c17694913eadafb9a07da22fb3736b2c";
+                    hash = "sha256-El6kdI1rOTWbwmX8bL1yB9KPwpew8GNK+lOb6h3KiOI=";
+                  };
+                }))
+              ];
+            };
         in {
-          # Run `nix fmt` to reformat the nix files
-          formatter = pkgs.nixfmt;
-
-          devShells.default = pkgs.mkShellNoCC {
-            name = "ufff";
-
-            buildInputs = [ php php.packages.composer pkgs.sphinx (pkgs.python3.withPackages (p: with p; [ pip ])) ];
-
-            shellHook = ''
-              # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
-              # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
-              export PIP_PREFIX=$(pwd)/build/pip_packages
-              export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
-              export PATH="$PIP_PREFIX/bin:$PATH"
-              unset SOURCE_DATE_EPOCH
-              pip install -r docs/requirements.txt
-            '';
+          _module.args.pkgs = import self.inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.nix-php-composer-builder.overlays.default ];
+            config.allowUnfree = true;
           };
+
+          devShells.default = pkgs.mkShellNoCC { buildInputs = [ (php "82") (php "82").packages.composer ]; };
+          devShells.php83 = pkgs.mkShellNoCC { buildInputs = [ (php "83") (php "83").packages.composer ]; };
         };
     };
 }
