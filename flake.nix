d0f985d9ef3a41a63f46b0820be71aea295474c9
Lars Strojny <lars@strojny.net>
2024-01-22 16:10:29 +0100

    Development environment for PHP 8.3 (#223)

--- flake.nix
+++ flake.nix
@@ -1,67 +1,59 @@
 {
-  description = "ufff";
-
+  description = "uffff";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nix-shell.url = "github:loophp/nix-shell";
-    nix-shell.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs = inputs@{ self, flake-parts, systems, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      # This flake is for Linux (x86) and Apple (darwin) systems
-      # If you need more systems, inspect `nixpkgs.lib.systems.flakeExposed` and
-      # add them to this list.
-      #
-      # $ nix repl "<nixpkgs>"
-      # nix-repl> lib.systems.flakeExposed
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = import systems;
 
-      perSystem = { pkgs, system, ... }:
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }:
         let
-          php = inputs.nix-shell.api.makePhp system {
-            php = "php82";
-            withExtensions = [
-              "ctype"
-              "curl"
-              "dom"
-              "filter"
-              "intl"
-              "mbstring"
-              "opcache"
-              "openssl"
-              "simplexml"
-              "tokenizer"
-              "xmlwriter"
-              "zlib"
-
-              # Development only
-              "xdebug"
-            ];
-
-            extraConfig = ''
-              xdebug.mode = coverage
-            '';
+          makePhp = version:
+            let phpPkg = pkgs."php${builtins.toString version}";
+            in phpPkg.buildComposerProject rec {
+              name = "lstrojny/uffff";
+              pname = name;
+              runtimeInputs = [ phpPkg ];
+              vendorHash = "sha256-xeurPFckxZjRGUj1bZ6OVQj+VkeHGw29aHOUDG7eOSE=";
+              version = "0.2.0";
+              src = inputs.self;
+              composerNoDev = false;
+              php = phpPkg.buildEnv {
+                extensions = ({ enabled, all }: enabled ++ (with all; [ xdebug ]));
+                extraConfig = "xdebug.mode=coverage";
+              };
+            };
+          phpVersions = [ 83 82 ];
+          makeShell = version:
+            let phpDrv = makePhp version;
+            in {
+              name = "php${builtins.toString version}";
+              value = pkgs.mkShellNoCC {
+                packages = [
+                  phpDrv
+                  phpDrv.php
+                  phpDrv.php.packages.composer
+                  pkgs.sphinx
+                  (pkgs.python3.withPackages (p: with p; [ pip ]))
+                ];
+                shellHook = ''
+                  # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+                  # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+                  export PIP_PREFIX=$(pwd)/build/pip_packages
+                  export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+                  export PATH="$PIP_PREFIX/bin:$PATH"
+                  unset SOURCE_DATE_EPOCH
+                  pip install -r docs/requirements.txt
+                '';
+              };
+            };
+          devShells = builtins.listToAttrs (map makeShell phpVersions);
+          devShellsWithDefault = devShells // {
+            default = devShells."php${builtins.toString (builtins.head phpVersions)}";
           };
-        in {
-          # Run `nix fmt` to reformat the nix files
-          formatter = pkgs.nixfmt;
-
-          devShells.default = pkgs.mkShellNoCC {
-            name = "ufff";
-
-            buildInputs = [ php php.packages.composer pkgs.sphinx (pkgs.python3.withPackages (p: with p; [ pip ])) ];
-
-            shellHook = ''
-              # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
-              # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
-              export PIP_PREFIX=$(pwd)/build/pip_packages
-              export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
-              export PATH="$PIP_PREFIX/bin:$PATH"
-              unset SOURCE_DATE_EPOCH
-              pip install -r docs/requirements.txt
-            '';
-          };
-        };
+        in { devShells = devShellsWithDefault; };
     };
 }
