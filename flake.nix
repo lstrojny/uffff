e670a6c605ff1eec00087208c0311448dd3859e4
Lars Strojny <lars@strojny.net>
2024-01-22 14:14:36 +0100

    Restore sphinx

--- flake.nix
+++ flake.nix
@@ -31,7 +31,24 @@
             let phpDrv = makePhp version;
             in {
               name = "php${builtins.toString version}";
-              value = pkgs.mkShellNoCC { packages = [ phpDrv phpDrv.php phpDrv.php.packages.composer ]; };
+              value = pkgs.mkShellNoCC {
+                packages = [
+                  phpDrv
+                  phpDrv.php
+                  phpDrv.php.packages.composer
+                  pkgs.sphinx
+                  (pkgs.python3.withPackages (p: with p; [ pip ]))
+                ];
+                shellHook = ''
+                  # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+                  # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+                  export PIP_PREFIX=$(pwd)/build/pip_packages
+                  export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+                  export PATH="$PIP_PREFIX/bin:$PATH"
+                  unset SOURCE_DATE_EPOCH
+                  pip install -r docs/requirements.txt
+                '';
+              };
             };
           devShells = builtins.listToAttrs (map makeShell phpVersions);
           devShellsWithDefault = devShells // {
